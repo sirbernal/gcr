10a4dc14ac9c4bd61d5d3d74e9cfe06186a4a4bc
Cristian Bernal <cristian.bernal@sansano.usm.cl>
2020-07-13 20:23:07 -0400

    first commit

---
+++ src/main/java/com/example/gcr/GcrApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gcr/GcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
