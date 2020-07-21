a3c5a7f011aa5808ece2f567592909518156db7b
Cristian Bernal <cristian.bernal@sansano.usm.cl>
2020-07-20 22:06:15 -0400

    update daos/services

--- src/main/java/com/example/gcr/services/QuimioterapiaService.java
+++ src/main/java/com/example/gcr/services/QuimioterapiaService.java
@@ -1,8 +1,27 @@
 package com.example.gcr.services;
-import com.example.gcr.models.Quimioterapia;
+
 import com.example.gcr.daos.QuimioterapiaRepository;
+import com.example.gcr.models.QuimioId;
+import com.example.gcr.models.Quimioterapia;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service("QuimioterapiaService")
 public class QuimioterapiaService {
+    @Autowired
+    private QuimioterapiaRepository quimioterapiaRepository;
+
+    public Quimioterapia saveUpdateQuimio(Quimioterapia quimioterapia){
+        return quimioterapiaRepository.save(quimioterapia);
+    }
+    public Quimioterapia findQuimioById(QuimioId quimioId){
+        return quimioterapiaRepository.findByQuimioId(quimioId);
+    }
+    public Iterable<Quimioterapia> listAllQuimio() {
+        return quimioterapiaRepository.findAll();
+    }
+    public void deleteQuimio(QuimioId quimioId){
+        quimioterapiaRepository.deleteByQuimioId(quimioId);
+    }
+
 }
