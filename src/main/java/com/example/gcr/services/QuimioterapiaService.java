4da429b918592703744177ae6ee2e22773d0e63b
Cristian Bernal <cristian.bernal@sansano.usm.cl>
2020-07-20 21:58:42 -0400

    update daos/services

--- src/main/java/com/example/gcr/services/QuimioterapiaService.java
+++ src/main/java/com/example/gcr/services/QuimioterapiaService.java
@@ -1,4 +1,8 @@
 package com.example.gcr.services;
+import com.example.gcr.models.Quimioterapia;
+import com.example.gcr.daos.QuimioterapiaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 public class QuimioterapiaService {
 }
